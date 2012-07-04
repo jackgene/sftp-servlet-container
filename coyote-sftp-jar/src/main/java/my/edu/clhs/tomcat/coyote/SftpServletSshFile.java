69074f5a4f5cc1f46c9648e7964b60d32bc2f844
Jack Leow <jackgene@gmail.com>
2012-07-04 09:45:28 -0700

    Significant refactoring to make code a little easier to understand.

---
+++ coyote-sftp-jar/src/main/java/my/edu/clhs/tomcat/coyote/SftpServletSshFile.java
@@ -0,0 +1,247 @@
+/*
+ * SftpServletSshFile.java
+ *
+ * Copyright 2011-2012 Jack Leow
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package my.edu.clhs.tomcat.coyote;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.apache.sshd.server.SshFile;
+
+class SftpServletSshFile implements SshFile {
+    private static final DateFormat RFC1123_DATE_FORMAT =
+        new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+    
+    private final SftpServletFileSystemView fileSystemView;
+    /**
+     * The path of this file. {@link File} is used here to take
+     * advantage of common file operations it provides, such as
+     * {@link File#getParentFile()}, {@link File#getAbsolutePath()}
+     * and {@link File#equals(Object)} to name a few.
+     */
+    private final File path;
+    private final String absolutePath;
+    private final String lastModifiedRfc1123;
+    private final boolean isFile;
+    private final boolean isDirectory;
+    private final long contentLength;
+    
+    private SftpServletSshFile(
+            SftpServletFileSystemView fileSystemView, String path,
+            String lastModifiedRfc1123, boolean isFile, boolean isDirectory,
+            long contentLength) {
+        this.fileSystemView = fileSystemView;
+        this.path = new File(path);
+        try {
+            this.absolutePath = this.path.getCanonicalPath();
+        } catch (IOException e) {
+            // TODO make my own runtime exception?
+            throw new RuntimeException(e);
+        }
+        this.lastModifiedRfc1123 = lastModifiedRfc1123;
+        this.isFile = isFile;
+        this.isDirectory = isDirectory;
+        this.contentLength = contentLength;
+    }
+    
+    public static class Builder {
+        private final SftpServletFileSystemView fileSystemView;
+        public Builder(SftpServletFileSystemView fileSystemView) {
+            this.fileSystemView = fileSystemView;
+        }
+        
+        private String path;
+        public Builder path(String path) {
+            this.path = path;
+            return this;
+        }
+        
+        private boolean isFile;
+        public Builder isFile(boolean isFile) {
+            this.isFile = isFile;
+            return this;
+        }
+        
+        private boolean isDirectory;
+        public Builder isDirectory(boolean isDirectory) {
+            this.isDirectory = isDirectory;
+            return this;
+        }
+        
+        // TODO rename to "size"
+        private long contentLength = 0L;
+        public Builder contentLength(long contentLength) {
+            this.contentLength = contentLength;
+            return this;
+        }
+        
+        private String lastModifiedRfc1123;
+        public Builder lastModifiedRfc1123(String lastModifiedRfc1123) {
+            this.lastModifiedRfc1123 = lastModifiedRfc1123;
+            return this;
+        }
+       
+        public SftpServletSshFile build() {
+            return new SftpServletSshFile(
+                fileSystemView, path, lastModifiedRfc1123,
+                isFile, isDirectory, contentLength);
+        }
+    }
+    
+    // @Override
+    public String getAbsolutePath() {
+        return absolutePath;
+    }
+    
+    // @Override
+    public String getName() {
+        return path.getName();
+    }
+    
+    // @Override
+    public void truncate() throws IOException {
+        // do nothing
+    }
+    
+    // @Override
+    public boolean setLastModified(long time) {
+        return false;
+    }
+    
+    // @Override
+    public boolean move(SshFile destination) {
+        throw new UnsupportedOperationException();
+    }
+    
+    // @Override
+    public boolean mkdir() {
+        throw new UnsupportedOperationException();
+    }
+    
+    // @Override
+    public List<SshFile> listSshFiles() {
+        return fileSystemView.getDirectoryContents(getAbsolutePath());
+    }
+    
+    // @Override
+    public boolean isWritable() {
+        return false;
+    }
+    
+    public boolean isExecutable() {
+        return isDirectory();
+    }
+    
+    // @Override
+    public boolean isRemovable() {
+        return false;
+    }
+    
+    // @Override
+    public boolean isReadable() {
+        return true;
+    }
+    
+    // @Override
+    public boolean isFile() {
+        return isFile;
+    }
+    
+    // @Override
+    public boolean isDirectory() {
+        return isDirectory;
+    }
+    
+    // @Override
+    public void handleClose() throws IOException {
+        // TODO close input/output streams
+        // do nothing
+    }
+    
+    // @Override
+    public long getSize() {
+        return contentLength;
+    }
+    
+    // @Override
+    public SshFile getParentFile() {
+        throw new UnsupportedOperationException();
+    }
+    
+    // @Override
+    public long getLastModified() {
+        final long lastModified;
+        
+        if (lastModifiedRfc1123 != null) {
+            try {
+                lastModified = RFC1123_DATE_FORMAT.
+                    parse(lastModifiedRfc1123).getTime();
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            lastModified = System.currentTimeMillis();
+        }
+        
+        return lastModified;
+    }
+    
+    // @Override
+    public boolean doesExist() {
+        return true; // TODO this makes it HttpServlet compatible.
+    }
+    
+    // @Override
+    public boolean delete() {
+        // TODO implement for HTTP DELETE.
+        throw new UnsupportedOperationException("Pending");
+    }
+    
+    public boolean create() throws IOException {
+        return true;
+    }
+    
+    // @Override
+    public OutputStream createOutputStream(long offset) throws IOException {
+        return fileSystemView.getFileOutputStream(getAbsolutePath());
+    }
+    
+    // @Override
+    public InputStream createInputStream(long offset) throws IOException {
+        InputStream is = fileSystemView.getFileInputStream(getAbsolutePath());
+        if (is != null) {
+            try {
+                is.skip(offset);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        
+        return is;
+    }
+    
+    @Override
+    public String toString() {
+        return path.toString();
+    }
+}
